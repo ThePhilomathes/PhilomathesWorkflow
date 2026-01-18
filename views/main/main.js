d71fa06ecda0bcb6a61f33994660607fbe88d7d0
Philomathes <lzeanallen@gmail.com>
2026-01-19 06:36:58 +0800

    Sidebar test

--- views/main/main.js
+++ views/main/main.js
@@ -6,21 +6,5 @@ const MainComponent = defineAsyncComponent(async() =>{
   }
 });
 
-   function toggle(id) {
-      document.getElementById(id).classList.toggle('hidden');
-    }
 
-    document.querySelectorAll('.section-header').forEach(header => {
-  header.addEventListener('click', () => {
-    const list = header.nextElementSibling;
-    list.classList.toggle('collapsed');
-  });
-});
 
-function toggleMenu(id) {
-    document.getElementById(id).classList.toggle('hidden');
-  }
-
-  function toggleSidebar() {
-    document.getElementById('sidebar').classList.toggle('-translate-x-full');
-  }
---
+++ views/main/sidebar.js
@@ -0,0 +1,7 @@
+function toggleMenu(id) {
+    document.getElementById(id).classList.toggle('hidden');
+  }
+
+  function toggleSidebar() {
+    document.getElementById('sidebar').classList.toggle('-translate-x-full');
+  }
