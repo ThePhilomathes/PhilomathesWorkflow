f129c3a40c42613779587922340d3cef75c4bd83
Philomathes <lzeanallen@gmail.com>
2026-01-26 22:03:35 +0800

    Changes to be committed: 	new file:   .env 	modified:   src/router/index.js 	modified:   vite.config.js
    Changes not staged for commit:
    	deleted:    .env
    
        -- updated router

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from '@router/routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/PhilomathesWorkflow/'),
   routes: routes,
 })
 
